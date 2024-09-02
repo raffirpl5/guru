7281dc70e4cc9df23ccbc148c297e58f00030459
gopi <muhammad_raffi_33rpl@student.smktelkom-mlg.sch.id>
2024-09-02 14:11:37 +0700

    tes

--- GuruTester.java
+++ GuruTester.java
@@ -8,31 +8,40 @@ public class GuruTester {
         System.out.println("Nama : ");
         String nama = input.nextLine();
         System.out.println("Mapel : ");
-        String id = input.nextLine();
+        String mapel = input.nextLine();
         System.out.println("Status: ");
         String Status = input.nextLine();
         System.out.println("NIP: ");
         int nip = input.nextInt();
         input.close();
 
-        KelasGuru guru = new KelasGuru(nip, nama, nama, Status);
+        // KelasGuru guru = new KelasGuru(nip, nama, nama, Status);
 
-       KelasGuru buPasha = new KelasGuru(2024, "Pasha", "erpeel", "tetap");
-       KelasGuru pakT = new KelasGuru(1945, " T", "Olahraga", "tetap");
-       KelasGuru  buAul = new KelasGuru(2025, "Aul", "erpeel", "tetap");
-       KelasGuru pakFirman = new KelasGuru(2232, "Firman", "BK", "tetap");
-       KelasGuru pakTulus = new KelasGuru(2222, "Tulus", "IPAS", "tetap");;
+    //    KelasGuru buPasha = new KelasGuru(2024, "Pasha", "erpeel", "tetap");
+    //    KelasGuru pakT = new KelasGuru(1945, " T", "Olahraga", "tetap");
+    //    KelasGuru  buAul = new KelasGuru(2025, "Aul", "erpeel", "tetap");
+    //    KelasGuru pakFirman = new KelasGuru(2232, "Firman", "BK", "tetap");
+    //    KelasGuru pakTulus = new KelasGuru(2222, "Tulus", "IPAS", "tetap");;
 
-        System.out.println("------------");
-        guru.print();
-        System.out.println("------------");
-        buPasha.print();
-        System.out.println("------------");
-        pakT.print();
-        System.out.println("------------");
-        buAul.print();
-        System.out.println("------------");
-        pakFirman.print();
+        // System.out.println("------------");
+        // guru.print();
+        // System.out.println("------------");
+        // buPasha.print();
+        // System.out.println("------------");
+        // pakT.print();
+        // System.out.println("------------");
+        // buAul.print();
+        // System.out.println("------------");
+        // pakFirman.print();
+
+        KelasGuru tes = new KelasGuru();
+        tes.setNama(nama);
+        tes.setMapel(mapel);
+        tes.setStatus(Status);
+        tes.setNip(nip);
+        System.out.println("--------");
+        tes.print();
+        System.out.println("--------");
 
       
 
--- KelasGuru.java
+++ KelasGuru.java
@@ -1,8 +1,50 @@
 package guru;
 
 public class KelasGuru {
-    int nip;
-    String nama, mapel, status;
+    //variable
+    private int nip;
+    private String nama;
+    private String mapel;
+    private String status;
+
+    public int getNip() {
+        return this.nip;
+    }
+
+    public void setNip(int nip) {
+        this.nip = nip;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getMapel() {
+        return this.mapel;
+    }
+
+    public void setMapel(String mapel) {
+        this.mapel = mapel;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public KelasGuru() {
+        nip = 0;
+        mapel = "NULL";
+        nama = "NULL";
+        status = "NULL";
+    }
     
     public KelasGuru(int p, String n, String m, String s) {
         nip = p;
